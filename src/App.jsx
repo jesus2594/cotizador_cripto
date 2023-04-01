52ca3af38930a211c622a1d27b53630118b6c10c
jesus2594 <jesusdavid890@hotmail.com>
2023-04-01 12:18:05 -0500

    Se agrega consulta a API y un Spinner de carga

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
 import Formulario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -41,6 +43,28 @@ font-size: 34px;
 
 function App() {
 
+  const [monedas, setMonedas] = useState({})
+  const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false )
+
+  useEffect(() => {
+    if(Object.keys(monedas).length > 0){
+      const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
+        const { moneda, criptomoneda } = monedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        setResultado(resultado.DISPLAY[criptomoneda][moneda])
+        setCargando(false)
+      }
+      cotizarCripto( )
+    }
+  }, [monedas])
+
   return(
     <Contenedor>
       <Imagen 
@@ -49,7 +73,12 @@ function App() {
       />
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
-        <Formulario />
+        <Formulario
+          setMonedas={setMonedas}
+        />
+        {cargando && <Spinner/>}
+        {resultado.PRICE && <Resultado resultado={resultado} />}
+
       </div>
     </Contenedor>
   )
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
+import Error from './Error'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
 
@@ -25,33 +26,60 @@ const InputSubmit = styled.input`
     }
 `
 
-const Formulario = () => {
+const Formulario = ({setMonedas}) => {
+  const [criptos, setCriptos] = useState([])
+  const [error, setError] = useState(false)
 
   const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
- /*  const [ SelectCriptomonedas ] = useSelectMonedas('Elige tu CriptoMoneda') */
+  const [ criptomoneda, SelectCriptomoneda ] = useSelectMonedas('Elige tu CriptoMoneda', criptos)
 
  useEffect(() => {
    const consultarAPI = async () => {
     const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
     const respuesta = await fetch(url)
     const resultado = await respuesta.json()
-    console.log(resultado)
+    const arrayCriptos = resultado.Data.map( cripto => {
+      const objeto = {
+        id: cripto.CoinInfo.Name,
+        nombre: cripto.CoinInfo.FullName
+      }
+      return objeto
+    })
+    setCriptos(arrayCriptos)
    }
    consultarAPI()
  }, [])
  
+ const handleSubmit = e => {
+  e.preventDefault()
+
+  if([moneda, criptomoneda].includes('')){
+    setError(true)
+    return
+  }
+
+  setError(false)
+  setMonedas({
+    moneda, criptomoneda
+  })
+ }
   
   return (
-    <form>
+    <>
+    {error && <Error>Todos los campos son obligatorios</Error> }
+
+    <form onSubmit={handleSubmit}>
 
       <SelectMonedas />
+      <SelectCriptomoneda />
       
-      {/* <SelectCriptomonedas /> */}
+      
         <InputSubmit 
             type="submit" 
             value="Cotizar" 
         />
     </form>
+    </>
   )
 }
 
