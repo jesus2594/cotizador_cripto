968b19eb809a56a8eca197b8bd6432bb48410da9
jesus2594 <jesusdavid890@hotmail.com>
2023-03-31 23:17:47 -0500

    Se agrega componente Formulario y hook useSelectMonedas

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+    margin-top: 30px;
+
+
+    &:hover {
+      background-color: #7A7DFE;
+      cursor: pointer;
+
+
+    }
+`
+
+const Formulario = () => {
+
+  const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+ /*  const [ SelectCriptomonedas ] = useSelectMonedas('Elige tu CriptoMoneda') */
+
+ useEffect(() => {
+   const consultarAPI = async () => {
+    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+    console.log(resultado)
+   }
+   consultarAPI()
+ }, [])
+ 
+  
+  return (
+    <form>
+
+      <SelectMonedas />
+      
+      {/* <SelectCriptomonedas /> */}
+        <InputSubmit 
+            type="submit" 
+            value="Cotizar" 
+        />
+    </form>
+  )
+}
+
+export default Formulario
